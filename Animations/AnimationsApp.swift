a9e1efec5e9f6eddcc893ebced611bfb3d3af698
ahadjawaid <ahadjawaid0@gmail.com>
2025-01-12 11:23:56 -0600

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by tk on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
